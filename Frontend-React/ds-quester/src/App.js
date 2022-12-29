82d5d76c4962bf35715aa00dc33fde7fc2227837
Sanaan Khalid <b2507417@ben.edu>
2022-12-29 15:08:51 -0600

    Auto stash before merge of "main" and "origin/main"

---
+++ Frontend-React/ds-quester/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import drugSearch from './drugSearch';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+   <drugSearch>Click me</drugSearch>
+ </div>
+  );
+}
+
+export default App;
